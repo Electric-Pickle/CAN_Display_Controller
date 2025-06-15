f2e08b952d6e5a80dac9856401f32519ea0a1eb7
Ethan Eigenraam <99495262+Electric-Pickle@users.noreply.github.com>
2025-06-15 03:07:04 -0700

    initial commit display working no can integration yet

---
+++ src/LVGL_Arduino.ino
@@ -0,0 +1,53 @@
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/get-started/platforms/arduino.html  */
+
+#include "Display_ST7789.h"
+#include "RTC_PCF85063.h"
+#include "Gyro_QMI8658.h"
+#include "LVGL_Driver.h"
+#include "ui.h"
+#include "PWR_Key.h"
+#include "LVGL_Example.h"
+#include "BAT_Driver.h"
+
+void DriverTask(void *parameter) {
+  while(1){
+    PWR_Loop();
+    BAT_Get_Volts();
+    PCF85063_Loop();
+    QMI8658_Loop(); 
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+void Driver_Loop() {
+  xTaskCreatePinnedToCore(
+    DriverTask,           
+    "DriverTask",         
+    4096,                 
+    NULL,                 
+    3,                    
+    NULL,                 
+    0                     
+  );  
+}
+void setup()
+{
+  PWR_Init(); // POWER LATCH/UNLATCH
+  BAT_Init(); // BAT VOLTAGE READOUT
+  I2C_Init(); // TOUCH ?!?
+  PCF85063_Init(); // RTC
+  QMI8658_Init(); // IMU
+  Backlight_Init(); // ...
+  LCD_Init();
+  Lvgl_Init();
+
+  ui_init();
+  // Lvgl_Example1();
+  Driver_Loop();
+}
+
+void loop()
+{
+  Lvgl_Loop();
+  vTaskDelay(pdMS_TO_TICKS(5));
+}
